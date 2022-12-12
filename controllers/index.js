09591fc95c7ff09801cde189ace4c23f2de1a354
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-12-11 22:05:49 -0800

    fixed controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,11 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
-// const indexRouter = require("./routes/index");
-// const usersRouter = require("./routes/users");
 
-// router.use("/", indexRouter);
+const homeRoutes = require("./home-routes");
+router.use("/", homeRoutes);
+
+// const usersRouter = require("./routes/users");
 // router.use("/users", usersRouter);
 
 module.exports = router;
